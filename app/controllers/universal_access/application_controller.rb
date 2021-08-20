48f64bc5030d8a69ebd6a0cf3671f60bc71ac20d
Ben Petro <benpetro@gmail.com>
2021-08-20 18:05:26 +1000

    rails 5 upgrade

--- app/controllers/universal_access/application_controller.rb
+++ app/controllers/universal_access/application_controller.rb
@@ -2,7 +2,7 @@ module UniversalAccess
   class ApplicationController < ::ApplicationController
     helper Universal::ApplicationHelper
     
-    before_filter :enforce_user_access
+    before_action :enforce_user_access
     
     def enforce_user_access
       render file: "#{Rails.root}/public/404.html", status: 404, layout: false if !universal_access_allowed
